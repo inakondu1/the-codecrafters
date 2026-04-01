6ede01851e31bd55acf3f55351dd8e76ef2abd74
inakondu1 <inaisah66@gmail.com>
2026-04-01 17:58:10 +0100

    Add files via upload

---
+++ sentinel_reborn/binTodecimal.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func binTodecimal(s string) string {
+	words := strings.Fields(s)
+
+	for i := 0; i < len(words); i++ {
+
+		switch words[i] {
+		case "(bin)":
+			value, err := strconv.ParseInt(words[i-1], 2, 64)
+			if err == nil {
+				words[i-1] = strconv.FormatInt(value, 10)
+			}
+			words = append(words[:i], words[i+1:]...)
+			i--
+		}
+	}
+	return strings.Join(words, " ")
+}
